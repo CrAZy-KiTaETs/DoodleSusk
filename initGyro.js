1812f207faf0ad7de5aecb8742fbb8903559418c
Crazy <crazy.kitaiets228@mail.ru>
2024-06-03 16:39:59 +0500

    new V

---
+++ initGyro.js
@@ -0,0 +1,37 @@
+
+let gamma = 0
+
+function handleOrientation(event) {
+  gamma = event.gamma;
+}
+ 
+export function init() {
+  if (
+    typeof DeviceOrientationEvent !== "undefined" &&
+    typeof DeviceOrientationEvent.requestPermission === "function"
+  ) {
+    DeviceOrientationEvent.requestPermission()
+      .then((permissionState) => {
+        if (permissionState === "granted") {
+          window.addEventListener("deviceorientation", handleOrientation);
+        } else {
+          alert("Разрешение на доступ к ориентации устройства было отклонено.");
+        }
+      })
+      .catch((error) => {
+        console.error(
+          "Ошибка запроса разрешения на ориентацию устройства:",
+          error
+        );
+        alert("Ошибка запроса разрешения на ориентацию устройства.");
+      });
+  } else if (window.DeviceOrientationEvent) {
+    // For browsers that do not require permission
+    window.addEventListener("deviceorientation", handleOrientation);
+  } else {
+    alert("Device Orientation API not supported.");
+  }
+  return gamma
+}
+
+
