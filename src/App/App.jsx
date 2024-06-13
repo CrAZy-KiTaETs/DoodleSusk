a4e33f3357e70638a667ecd49438fa4b298ec6d5
Crazy <crazy.kitaiets228@mail.ru>
2024-06-13 18:09:35 +0500

    open webapp with link

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -56,42 +56,74 @@ export function App() {
   useEffect(() => {
     const tg = window.Telegram?.WebApp;
     let init = tg.initDataUnsafe.user;
+    let params = tg.initDataUnsafe.start_param;
     if (init) {
       console.log(tg, init, "это тг");
-      // add(init);
-      get()
+      add(init);
     } else {
       console.log("Подключения нет");
     }
+    // find()
   }, []);
 
   const add = async (tg) => {
-    let user = {
-      id: tg.id,
-      username: tg.username,
-      ref: "3sda3sd",
-      wallet: "sad",
-      balance: "0",
-      invited: "false",
-      is_sub: "false",
-      ref_count: "0",
-      twitter: "asdw3f",
-      inf: "false",
-      inf_sub: '"false"',
-      inf_link: "",
-    };
-    let res = await axios.post("http://localhost:4000/users/add/", user, {
-      headers: { "Content-Type": "application/json" },
-    });
-    console.log(res, "тип отправил данные");
+    try {
+      let user = await find();
+      if (!user) {
+        let newUser = {
+          id: 714289599,
+          username: 'crazy',
+          ref: "https://t.me/ElonSusk_bot?start=73b6b064-ce77-4273-ba59-617fe85646a3",
+          wallet: "",
+          balance: 0,
+          invited: "false",
+          is_sub: "false",
+          ref_count: 0,
+          twitter: "",
+          inf: "false",
+          inf_sub: "true",
+          inf_link: 1111,
+        };
+        let res = await axios.post(
+          "http://localhost:4000/users/add/",
+          newUser,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
+        return console.log(res, "Пользователь создан");
+      }
+      console.log("Пользователь уже существует");
+
+    } catch (error) {
+      console.log("ошибка при создании пользователя", error);
+    }
   };
 
   const get = async () => {
     let res = await axios.get("http://localhost:4000/users/");
-    console.log(res, "uusers");
+    console.log(res.data, "uusers");
+  };
+
+  const find = async () => {
+    let res = await axios.get("http://localhost:4000/users/findById/714289599");
+    console.log(res.data, "find");
+    return res.data;
+  };
+
+  const findRef = async () => {
+    let res = await axios.get("http://localhost:4000/users/findByRef/73b6b064-ce77-4273-ba59-617fe85646a3");
+    console.log(res.data, "find");
+    return res.data;
   };
   return (
     <div className="app-container">
+      <div className="btn_wrapper">
+        <button onClick={() => add()}>ADD</button>
+        <button onClick={() => get()}>get</button>
+        <button onClick={() => find()}>findId</button>
+        <button onClick={() => findRef()}>findRef</button>
+      </div>
       {!user ? (
         <>
           <LoadingPage userIsReady={userIsReady} />
