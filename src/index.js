7879cecc1120c1afd482828a6d62fb9b589fa89f
Crazy <crazy.kitaiets228@mail.ru>
2024-06-20 14:57:36 +0500

    fix port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require("cors")
 const app = express();
-const port = 80;
+const port = process.env.port || 8080;
 
 // Middleware
 app.use(bodyParser.json());
